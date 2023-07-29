e150ea9a3af46b8a6a364b2692a9e48e559985fc
lahirumenik <e19236@eng.pdn.ac.lk>
2023-07-29 13:17:19 +0530

    add jwt and spring security by config,entity,service, repo, filter layers

---
+++ backend/src/main/java/com/bidCircle/backend/entity/UserInfo.java
@@ -0,0 +1,35 @@
+package com.bidCircle.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(name = "UniqueUserName", columnNames = {"userName"})})
+@Builder
+public class UserInfo {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_info_seq")
+    @SequenceGenerator(name = "user_info_seq", sequenceName = "user_info_seq", allocationSize = 1)
+    private long id;
+    @Column(unique = true)
+    private String userName;
+    private String password;
+    private String roles;
+    private String email;
+    private String birthDate; //format: dd/mm/year
+    private String name;
+    private Date startDate;
+    private String nic;
+
+
+}
